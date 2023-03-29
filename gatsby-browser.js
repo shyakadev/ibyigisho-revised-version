2ed44686d471892c824d6f4e9a0dd979bf89560d
shyakadev <shyakadev@gmail.com>
2023-03-29 19:01:48 +0200

    fix: reload on page update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,6 @@ import "./src/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
+
+// Reload on page update
+exports.onServiceWorkerUpdateReady = () => window.location.reload(true);
